0d07fd0c8c51503638d0522272b23c0ab4451146
zmij <sergei.a.fedorov@gmail.com>
2019-11-10 01:59:24 +0300

    AK47 example and presentation draft

---
+++ examples/machine_gun.cpp
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2019 Sergei A. Fedorov
+ * machine_gun.cpp
+ *
+ *  Created on: Nov 4, 2019
+ *      Author: ser-fedorov
+ */
+
+#include "machine_gun.hpp"
+
+#include "../test/test_observer.hpp"
+
+#include <iostream>
+
+using machine_gun
+    = afsm::state_machine<guns::machine_gun_def, afsm::none, afsm::test::test_fsm_observer>;
+
+int
+main(int, char*[])
+{
+    machine_gun mg;
+
+    mg.set_on_emit_bullet([]() { std::clog << "pew\n"; });
+    mg.set_on_reload([]() { std::clog << "reload\n"; });
+    mg.set_on_empty([]() { std::clog << "ammo depleted\n"; });
+    mg.set_on_safety_change([](guns::safety_lever v) { std::clog << to_chars(v) << "\n"; });
+
+    mg.make_observer("AK47");
+    mg.process_event(afsm::none{});
+    mg.process_event(guns::events::safety_lever_down{});
+    mg.process_event(guns::events::reload{});
+    mg.process_event(guns::events::trigger_pull{});
+    for (auto i = 0; i < 15; ++i) {
+        mg.process_event(guns::events::tick{});
+    }
+    mg.process_event(guns::events::trigger_release{});
+
+    return 0;
+}
---
+++ examples/machine_gun.hpp
@@ -0,0 +1,258 @@
+/**
+ * machine_gun.hpp
+ *
+ *  Created on: Nov 9, 2019
+ *      Author: ser-fedorov
+ */
+
+#ifndef EXAMPLES_MACHINE_GUN_HPP_
+#define EXAMPLES_MACHINE_GUN_HPP_
+
+#include <afsm/fsm.hpp>
+
+#include <functional>
+#include <type_traits>
+
+namespace guns {
+
+namespace events {
+
+struct trigger_pull {};
+struct trigger_release {};
+
+struct reload {};
+
+struct safety_lever_up {};
+struct safety_lever_down {};
+
+struct tick {};
+
+}    // namespace events
+
+// Guards fwd
+struct depleted;
+struct firing;
+
+// Actions fwd
+struct emit_bullet;
+struct change_magazine;
+struct update_safety;
+
+enum class safety_lever { safe, automatic, single };
+
+struct machine_gun_def : afsm::def::state_machine<machine_gun_def> {
+    using fsm_type = afsm::state_machine<machine_gun_def>;
+
+    //@{
+    /** @name States */
+    struct selector : state_machine<selector> {
+        struct safe : state<safe> {
+            static constexpr safety_lever lever = safety_lever::safe;
+        };
+        struct single : state<single> {
+            static constexpr safety_lever lever = safety_lever::single;
+        };
+        struct automatic : state<automatic> {
+            static constexpr safety_lever lever = safety_lever::automatic;
+        };
+
+        using initial_state = safe;
+
+        using down = events::safety_lever_down;
+        using up   = events::safety_lever_up;
+
+        // clang-format off
+        // selector transition table
+        using transitions = transition_table<
+        /*  State       | Event | Next      | Action        | Guard         */
+        tr< safe        , down  , automatic , update_safety , none          >,
+        tr< automatic   , up    , safe      , update_safety , not_<firing>  >,
+        tr< automatic   , down  , single    , update_safety , not_<firing>  >,
+        tr< single      , up    , automatic , update_safety , not_<firing>  >
+        >;
+        // clang-format on
+    };
+
+    struct trigger : state_machine<trigger> {
+        // To shorten the transition table
+        using pull    = events::trigger_pull;
+        using release = events::trigger_release;
+        using reload  = events::reload;
+        using tick    = events::tick;
+
+        struct init : state<init> {};
+        struct ready : state<ready> {};
+        struct empty : state<empty> {
+            template <typename FSM>
+            void
+            on_enter(afsm::none&&, FSM& fsm)
+            {
+                root_machine(fsm).ammo_depleted();
+            }
+        };
+        struct fire : state<fire> {};
+
+        using initial_state = init;
+        // clang-format off
+        // trigger transition table
+        using transitions = transition_table<
+        /*  State   | Event     | Next      | Action            | Guard                             */
+        tr< init    , none      , ready     , none              , not_<depleted>                    >,
+        tr< init    , none      , empty     , none              , depleted                          >,
+        tr< empty   , reload    , ready     , change_magazine   , none                              >,
+        tr< ready   , pull      , fire      , emit_bullet       , not_<in_state<selector::safe>>    >,
+        tr< ready   , reload    , ready     , change_magazine   , none                              >,
+        tr< fire    , release   , ready     , none              , none                              >,
+        tr< fire    , tick      , fire      , emit_bullet       , and_<in_state<selector::automatic>>,
+                                                                       not_<depleted>>,
+        tr< fire    , none      , empty     , none              , depleted                          >
+        >;
+        // clang-format on
+    };
+    using orthogonal_regions = type_tuple<trigger, selector>;
+    //@}
+
+    using callback        = std::function<void()>;
+    using safety_callback = std::function<void(safety_lever)>;
+
+    bool
+    empty() const
+    {
+        return ammo_ == 0;
+    }
+    int
+    ammo() const
+    {
+        return ammo_;
+    }
+
+    void
+    set_on_reload(callback cb)
+    {
+        on_reload_ = cb;
+    }
+
+    void
+    set_on_emit_bullet(callback cb)
+    {
+        on_emit_bullet_ = cb;
+    }
+
+    void
+    set_on_empty(callback cb)
+    {
+        on_empty_ = cb;
+    }
+
+    void
+    set_on_safety_change(safety_callback cb)
+    {
+        on_change_safety_ = cb;
+    }
+
+    void
+    reload()
+    {
+        ammo_ = 30;
+        if (on_reload_)
+            on_reload_();
+    }
+
+    void
+    emit_bullet()
+    {
+        --ammo_;
+        if (on_emit_bullet_)
+            on_emit_bullet_();
+    }
+
+    void
+    update_safety(safety_lever s)
+    {
+        if (on_change_safety_) {
+            on_change_safety_(s);
+        }
+    }
+
+    void
+    ammo_depleted()
+    {
+        if (on_empty_) {
+            on_empty_();
+        }
+    }
+
+private:
+    int ammo_ = 0;
+
+    callback on_emit_bullet_;
+    callback on_empty_;
+    callback on_reload_;
+
+    safety_callback on_change_safety_;
+};
+
+using machine_gun = afsm::state_machine<machine_gun_def>;
+
+//@{
+/** @name Guards */
+struct depleted {
+    template <typename FSM, typename State>
+    bool
+    operator()(FSM const& fsm, State const&) const
+    {
+        return root_machine(fsm).empty();
+    }
+};
+
+struct firing : machine_gun_def::in_state<machine_gun_def::trigger::fire> {};
+//@}
+
+//@{
+struct emit_bullet {
+    template <typename Event, typename FSM>
+    void
+    operator()(Event const&, FSM& fsm) const
+    {
+        root_machine(fsm).emit_bullet();
+    }
+};
+
+struct change_magazine {
+    template <typename Event, typename FSM>
+    void
+    operator()(Event const&, FSM& fsm) const
+    {
+        root_machine(fsm).reload();
+    }
+};
+
+struct update_safety {
+    template <typename Event, typename FSM, typename SourceState, typename TargetState>
+    void
+    operator()(Event const&, FSM& fsm, SourceState&, TargetState&) const
+    {
+        root_machine(fsm).update_safety(TargetState::lever);
+    }
+};
+//@}
+
+using machine_gun = afsm::state_machine<machine_gun_def>;
+
+constexpr char const*
+to_chars(safety_lever v)
+{
+    switch (v) {
+    case safety_lever::safe:
+        return "safe";
+    case safety_lever::automatic:
+        return "auto";
+    case safety_lever::single:
+        return "sngl";
+    }
+    return "";
+}
+
+}    // namespace guns
+
+#endif /* EXAMPLES_MACHINE_GUN_HPP_ */
